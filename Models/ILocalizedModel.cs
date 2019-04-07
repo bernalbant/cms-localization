ddad1221a31631a0abf208de441e704a18b0b027
berkay ernalbant <berkayernalbant@gmail.com>
2019-04-07 16:29:27 +0300

    [feat] add content and localized model

---
+++ Models/ILocalizedModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace CmsLocalization.Helpers
+{
+    public interface ILocalizedModel
+    {
+    }
+
+    public interface ILocalizedModel<T> : ILocalizedModel where T : ILocalizedModelLocal
+    {
+        IList<T> Locales { get; set; }
+    }
+
+    public interface ILocalizedModelLocal
+    {
+        int LanguageId { get; set; }
+    }
+}
